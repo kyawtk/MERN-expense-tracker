04593fb676715ade466cfc91f21a95e12905d45b
Kyaw Thant Khine <kyawthantkhaing@gmail.com>
2020-11-08 13:28:47 +0630

    polishing

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const connectDb = require('./db')
-const transactionRoute =  require('./router/transactions')
-const cors  = require('cors')
+const express = require("express");
+const connectDb = require("./db");
+const path  = require('path')
+const transactionRoute = require("./router/transactions");
+const cors = require("cors");
 
-connectDb()
+connectDb();
 
 const app = express();
-app.use(express.json())
-app.use(cors())
-app.use('/transactions',transactionRoute)
+app.use(express.json());
+app.use(cors());
+app.use("/transactions", transactionRoute);
 
+app.use(express.static("client/build"));
 
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+);
 
-
-
-const PORT =process.env.port || 5000
-app.listen(PORT , console.log(`server listening on ${PORT}`))
+const PORT = process.env.port || 5000;
+app.listen(PORT, console.log(`server listening on ${PORT}`));
